45835111fd1ef3b297f7e27409d25b0b829795d0
hasanaydin <gecegez@gmail.com>
2022-08-26 20:56:24 +0300

    mongodb test

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dbConfig = require("./config/db.config");
 
 const app = express();
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+}).then(
+    () => {
+        console.log('connected');
+    },
+    (error) => {
+        console.log(error)
+    }
+)
+
 app.use(express.json());
 
 
